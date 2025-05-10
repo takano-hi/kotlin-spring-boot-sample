081907c7215c997468028ff7701f3209128ba4af
Hiroki Takano <midwhite.development@gmail.com>
2025-05-11 08:29:03 +0900

    load kotlin DSL from external annotations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
 	kotlin("plugin.jpa") version "1.9.25"
 }
 
-group = "com.example"
+group = "com.example.sample"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -46,4 +48,10 @@ allOpen {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+	}
 }
