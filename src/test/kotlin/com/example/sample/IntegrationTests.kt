1946a8f8c808932e57cce3e96ea106a7a31e6e46
Hiroki Takano <midwhite.development@gmail.com>
2025-05-11 11:04:13 +0900

    add test for ping endpoint

---
+++ src/test/kotlin/com/example/sample/IntegrationTests.kt
@@ -0,0 +1,19 @@
+package com.example.sample
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+    @Test
+    fun `should respond ping-pong`() {
+        val entity = restTemplate.getForEntity<String>("/")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>pong</h1>")
+    }
+}
