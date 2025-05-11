8a4df44f072b671de234a6efdcb8b4982f5d633f
Hiroki Takano <midwhite.development@gmail.com>
2025-05-11 12:04:12 +0900

    enable to run and connect to local MySQL

---
+++ README.md
@@ -0,0 +1,7 @@
+## how to run and connect to DB
+
+```sh
+$ docker compose up -d
+$ mysql -h 127.0.0.1 -P 3306 -u sample -p experts
+# password: password
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
+	runtimeOnly("mysql:mysql-connector-java:8.0.33")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
